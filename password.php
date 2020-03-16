cf7179d248b06c07c5b819862b657bdb3ad0a823
satirthaKreative <satirtha.kreative@gmail.com>
2020-03-16 11:21:55 +0530

    email  page design

---
+++ password.php
@@ -0,0 +1,40 @@
+<?php include ("inc/header.php") ?>
+<section class="inner-banner">
+   <div class="container">
+      <h1>Set Password</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>Login</li>
+         </ul>
+      </div>
+   </div>
+</section>
+<!-- end of inner-banner -->
+<section class="login-content">
+   <div class="container">
+      <div class="row">
+         <div class=" offset-3 col-sm-6 login-box cst-login">
+            <h1>Set Your Password</h1>
+            <form method="post">
+               <div class="form-group">
+                  <input type="password" name="user" class="form-control" placeholder="Type Password" value="" required="" />
+               </div>
+               <div class="form-group">
+                   <input type="password" name="user" class="form-control" placeholder="Confirm Password" value="" required="" />
+               </div>               
+               <div class="form-group">
+                  <input type="submit" name="LGform2" class="sign in" value="Set Password" />                 
+               </div>
+            </form>
+            
+           
+         </div>
+         <!-- end of col-sm-6 -->
+         
+     
+      </div>
+   </div>
+</section>
+<!-- end of theme-content -->
+<?php include ("inc/footer.php") ?>
