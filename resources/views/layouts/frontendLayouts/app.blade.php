d249a0af16bbedd8e3cf4478673fe4dfbb546f34
satirthaKreative <satirtha.kreative@gmail.com>
2020-03-21 13:53:33 +0530

    dac

--- resources/views/layouts/frontendLayouts/app.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-   	@include('frontend.include.head')
-<body id="go-top">
-	@include('frontend.include.header')
-    @yield('content')
-    @include('frontend.include.footer')
-</body>
-</html>
