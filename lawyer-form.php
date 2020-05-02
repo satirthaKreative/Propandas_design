f37b024b43f39a6a46b8a4c12fe67311dfb2c2b6
satirthaKreative <satirtha.kreative@gmail.com>
2020-05-02 14:21:45 +0530

    step lawyer page design

---
+++ lawyer-form.php
@@ -0,0 +1,115 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Search Lawyer</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">             
+               <div class="dsbrd-qststep">
+                        <form action="">
+                           <div class="step-structure">
+                              <div class="step-box">
+                                 <h5 class="qst">What kind of startup assistance do you need?</h5>
+                                 <ul class="stp-list">
+                                 <li>
+                                    <label><input type="checkbox" name="remember">Business Formation and Structure</label>    
+                                 </li>
+
+                                 <li>
+                                    <label><input type="checkbox" name="remember">Contracts or Agreements</label>    
+                                 </li>
+
+                                 <li>
+                                    <label><input type="checkbox" name="remember">Employment</label>    
+                                 </li>
+                              </ul>                                 
+                              </div>
+
+                              <div class="step-box">
+                                  <h5 class="qst">Would you like to receive text (SMS) notifications</h5>
+                                   <ul class="stp-list">
+                                     <li>
+                           <label><input type="radio" name="optradio">Yes</label>    
+                        </li>
+                        <li>
+                           <label><input type="radio" name="optradio">No</label>    
+                        </li>
+                                   </ul>
+                              </div>
+
+                              <div class="step-box">
+                                  <h5 class="qst">Which agreements would you like to discuss with a lawyer?</h5>
+                                    <input type="text" name="twitter" placeholder="Name">
+                                    <input type="email" name="facebook" placeholder="Email">
+                                    <input type="text" name="gplus" placeholder="Phone Number">
+                                    <input type="password" name="pass" placeholder="Password">
+                                    <input type="password" name="cpass" placeholder="Confirm Password">
+                                    <textarea name="" id="" placeholder="Message"></textarea>
+                              </div>
+
+                              <div class="step-box">
+                           <h5 class="qst">What will you need</h5>
+
+                            <select name="" >
+                        <option value="1">Select</option>
+                        <option value="1"> MTGA</option>
+                        <option value="2">MTGO</option>
+                        <option value="3">Cockatrice</option>
+                        <option value="4">Xmage</option>
+                     </select>
+                     <select name="" multiple="multiple" id="multy-select">
+                        <option value="1"> MTGA</option>
+                        <option value="2">MTGO</option>
+                        <option value="3">Cockatrice</option>
+                        <option value="4">Xmage</option>
+                     </select>
+                     <select name="" multiple  id="search-select">
+                        <option value="1"> MTGA</option>
+                        <option value="2">MTGO</option>
+                        <option value="3">Cockatrice</option>
+                        <option value="4">Xmage</option>
+                     </select>
+                              </div>
+
+                              <input type="submit" name="" value="Submit" class="flt-search">
+                              
+                           </div>
+                           
+                           
+                        </form>
+                     </div>               
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
