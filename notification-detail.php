72eb336e7557a291eca58f85e20f28d8d226b288
satirthaKreative <satirtha.kreative@gmail.com>
2020-05-07 10:39:45 +0530

    notification pages  page design

---
+++ notification-detail.php
@@ -0,0 +1,72 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Notification</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+
+              <div class="detail-content notf-detail">
+                <div class="row">
+                  <div class="col-md-8">
+                     <h5>Satirtha Das</h5>                
+                 <h6><i class="fa fa-envelope" aria-hidden="true"></i>Satirthadas@gmail.com</h6>
+                 <h6><i class="fa fa-phone" aria-hidden="true"></i>+917845127845</h6>
+                  </div>
+                  <div class="col-md-4">
+                    <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                  </div>
+                </div>             
+                
+                 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non.</p>
+                 <p><strong>Anim id est laborum</strong></p>
+                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non.</p>
+                 <ul>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                              </ul>
+
+                              <p>
+                                 <span><a href="javascript:void(0)" class="dt-btn">Back</a></span>                                
+                                 </p>
+              </div>                       
+               
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
---
+++ notification.php
@@ -0,0 +1,176 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Notification</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+                <h6>Notification</h6>     
+               <div class="dshbrd-inner"> 
+                        
+               <ul class="filter-result notification-list">
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                  	<div class="left-step">                        
+                  		<div class="media">
+                  			 <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                  			  <div class="media-body">
+                  			  	 <h5>At vero eos et accusamus et iusto odio</h5>
+                  			  	 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                  			  </div>
+                  		</div>
+                  	</div>
+
+                  	 <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                     <div class="left-step">                        
+                        <div class="media">
+                            <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                             <div class="media-body">
+                               <h5>Quis autem vel eum iure</h5>
+                               <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                             </div>
+                        </div>
+                     </div>
+
+                      <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                     <div class="left-step">                        
+                        <div class="media">
+                            <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                             <div class="media-body">
+                               <h5>Numquam eius modi tempora incidunt</h5>
+                               <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                             </div>
+                        </div>
+                     </div>
+
+                      <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                     <div class="left-step">                        
+                        <div class="media">
+                            <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                             <div class="media-body">
+                               <h5>Et harum quidem rerum facilis</h5>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                             </div>
+                        </div>
+                     </div>
+
+                      <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                     <div class="left-step">                        
+                        <div class="media">
+                            <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                             <div class="media-body">
+                               <h5>Et harum quidem rerum facilis</h5>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                             </div>
+                        </div>
+                     </div>
+
+                      <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+
+                  <li>
+                      <a href="javascript:void(0)" class="flex-anchore">
+                     <div class="left-step">                        
+                        <div class="media">
+                            <div class="close-circle"><i class="fa fa-times" aria-hidden="true"></i></div>                           
+                             <div class="media-body">
+                               <h5>Et harum quidem rerum facilis</h5>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                             </div>
+                        </div>
+                     </div>
+
+                      <div class="right-step">                     
+                           <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                        </div> 
+                     </a>
+                    
+                  </li>
+                  
+               </ul>
+            </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
