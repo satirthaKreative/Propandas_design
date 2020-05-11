3304553b17673896e237e9ffd6a62de209bfe1d3
satirthaKreative <satirtha.kreative@gmail.com>
2020-05-11 09:56:36 +0530

    proposal page design

---
+++ client-job.php
@@ -0,0 +1,181 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">My Jobs</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+               <h6>Recent Jobs</h6>
+               <div class="src-filter ">
+                        <form action="">
+                           <div class="row">
+                              <div class="col-md-4 plr-5">
+                                 <div class="form-group">
+                                    <label for="">Filter Jobs</label>
+                                    <select name="type" id="type" class="form-control" aria-hidden="true">
+                                       <option selected="selected" value="mine">Select Job Type</option>
+                                       <option value="">Business</option>
+                                       <option value="">Intellectual Property</option>
+                                       <option value="">Employment </option>
+                                       <option value="">Contracts &amp; Agreements</option>
+                                       <option value="">Immigration </option>
+                                       <option value="">Real Estate </option>
+                                       <option value="">Tax </option>
+                                       <option value="">Lawsuits &amp; Disputes</option>
+                                    </select>
+                                 </div>
+                              </div>
+                              <div class="col-md-4 plr-5">
+                                <div class="form-group">
+                                    <label for="">Search Jobs</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="Search for jobs by title...">
+                                 </div>
+                              </div>
+                              
+
+                               <div class="col-md-4 plr-5">
+                                <div class="form-group">
+                                    <label for="">&nbsp;</label>
+                                    <input type="submit" name="" value="Search" class="flt-search">
+                                 </div>
+                              </div>
+                           </div>
+                           
+                        </form>
+                     </div>
+
+                     <div class="dshbrd-inner">
+                        <ul class="filter-result"> 
+
+                        <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Trademark Search Services</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>29/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>
+
+                  <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Et harum quidem rerum facilis</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>27/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>
+
+
+                  <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Quis autem vel eum iure</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>26/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>                
+                  <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Trademark Search Services</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>25/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>
+
+                  <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Et harum quidem rerum facilis</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>
+
+
+                  <li>
+                      <div class="left-step">
+                        <h5><a href="client-single-job.php">Quis autem vel eum iure</a></h5>
+                               <p class="dg-text"><span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>19/04/2020
+                           </span></p>
+                               <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                     </div>
+
+                      <div class="right-step">
+                           <p><a href="client-single-job.php"><label>10</label>Proposals</a></p>
+                        </div>                       
+                   
+                  </li>
+
+                 
+
+                 
+               </ul>
+                        
+                     </div>               
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
---
+++ proposal.php
@@ -0,0 +1,175 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Search Lawyer</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+               <h6>Search a Lawyer</h6>
+               <div class="src-filter ">
+                        <form action="">
+                           <div class="row">
+                              <div class="col-md-3 plr-5">
+                                 <div class="form-group">
+                                    <label for="">Budget</label>
+                                  <input type="text" name="" id="" class="form-control" placeholder="Enter budget">
+                                 </div>
+                              </div>
+                              <div class="col-md-3 plr-5">
+                                <div class="form-group">
+                                    <label for="">Countries</label>
+                                    <select name="type" id="type"  class="form-control" aria-hidden="true">
+                                       <option selected="selected" value="mine">Select Country</option>
+                                       <option value="">Germany</option>
+                                       <option value="">Austria</option>                                      
+                                    </select>
+                                 </div>
+                              </div>
+                              <div class="col-md-3 plr-5">
+                                <div class="form-group">
+                                    <label for="">Popular Cities</label>
+                                    <select name="type" id="type"  class="form-control" aria-hidden="true">
+                                       <option selected="selected" value="mine">Select Cities</option>
+                                       <option value="">Berlin</option>
+                                       <option value="">Munich</option> 
+                                        <option value="">Duesseldorf</option>
+                                       <option value="">Koeln</option>   
+                                        <option value="">Hamburg</option>
+                                       <option value="">Hannover</option>
+                                       <option value="">Stuttgart</option>
+                                       <option value="">Frankfurt</option>                                          
+                                    </select>
+                                 </div>
+                              </div>
+
+                               <div class="col-md-3 plr-5">
+                                <div class="form-group">
+                                    <label for="">&nbsp;</label>
+                                    <input type="submit" name="" value="Search" class="flt-search">
+                                 </div>
+                              </div>
+                           </div>
+                           
+                        </form>
+                     </div>
+               <ul class="filter-result">                 
+                  <li>
+                  	<div class="left-step">
+                  		<div class="media">
+                  			 <img class="md-img" src="https://image.freepik.com/free-photo/smiling-mature-lawyer-working-courtroom_23-2147898545.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h6>Robert Hooks</h6> 
+
+                               <h6 class="highlight-list">
+                                 <span>$80/per hr</span>
+                                 <span class="date-text"><i class="fa fa-clock-o" aria-hidden="true"></i>29/04/2020</span>
+                              </h6>                 			  	 
+                                                        
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  	</div>
+
+                  	<div class="right-step">
+                  		<br>
+                           <a href="Lawyer-detail.php" class="shrt-btn vw-btn">View Details</a>
+                        </div>  
+                    
+                  </li>
+
+
+                  <li>
+                  	<div class="left-step">
+                  		<div class="media">
+                  			 <img class="md-img" src="https://www.bestattorneysofamerica.com/wp-content/uploads/cache/images/for-attorneys-woman/for-attorneys-woman-1416831204.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>Sara Najjar</h5>
+                  			  	 <p class="dg-text"><em>Tax Lawyers</em></p>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  	</div>
+
+                  	<div class="right-step">
+                  		<br>
+                           <a href="Lawyer-detail.php" class="shrt-btn vw-btn">View Details</a>
+                        </div>  
+                    
+                  </li>
+
+                  <li>
+                  	<div class="left-step">
+                  		<div class="media">
+                  			 <img class="md-img" src="https://www.jangattorneys.com/wp-content/themes/jang-2017/img/defaultinnerimgmob.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>Mitri Valentine </h5>
+                  			  	 <p class="dg-text"><em>Immigration Lawyers</em></p>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  	</div>
+
+                  	<div class="right-step">
+                  		<br>
+                           <a href="Lawyer-detail.php" class="shrt-btn vw-btn">View Details</a>
+                        </div>  
+                    
+                  </li>
+
+                  <li>
+                  	<div class="left-step">
+                  		<div class="media">
+                  			 <img class="md-img" src="https://freedomlaw.ca/wp-content/uploads/2018/06/corporate.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>William David.</h5>
+                  			  	 <p class="dg-text"><em>Real Estate Lawyers</em></p>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  	</div>
+
+                  	<div class="right-step">
+                  		<br>
+                           <a href="Lawyer-detail.php" class="shrt-btn vw-btn">View Details</a>
+                        </div>  
+                    
+                  </li>
+
+                  
+                  
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
