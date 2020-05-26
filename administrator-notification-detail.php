5daa483d49136e1ec750838fd10a666d56a8d092
satirthaKreative <satirtha.kreative@gmail.com>
2020-05-27 01:19:52 +0530

    system notify page design

---
+++ administrator-notification-detail.php
@@ -0,0 +1,79 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Administrator Trade </h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+
+              <div class="detail-content notf-detail">
+                <div class="row">
+                  <div class="col-md-8">
+                     <h5>At vero eos et accusamus et iusto odio</h5> 
+                     <h6>Quis nostrud exercitation ullamco laboris nisi ut aliquip</h6>              
+                 
+                  </div>
+                  <div class="col-md-4">
+                    <span class="date-text">
+                              <i class="fa fa-clock-o" aria-hidden="true"></i>20/04/2020
+                           </span>
+                  </div>
+                </div>                 
+                 <p><strong>Description</strong></p>
+                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non.</p>
+
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non.</p>
+                 <ul>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                                 <li>Sunt in culpa qui officia deserunt mollit anim</li>
+                              </ul>
+
+                              <p>
+                                 <span><a href="javascript:void(0)" class="dt-btn">Back</a></span>                                
+                                 </p>
+
+                                 <div class="reply-form">
+                                    <h5>Your Comment</strong></h5>
+                                    <form>
+                                      <textarea name="" id="" placeholder="Your Message"></textarea>
+                                      <input type="submit" name="" value="Submit" class="flt-search">
+                                    </form>
+                                 </div>
+              </div>                       
+               
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
---
+++ client-after-job-post.php
@@ -0,0 +1,145 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">My Jobs</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">              
+               <div class="detail-content">
+                   <div class="media ">                         
+                             <div class="media-body">
+                               <h5>Business Lawyers</h5>
+                               <h6>Laboris nisi ut aliquip ex ea. </h6>
+                               <p>Ut enim ad minim veniam,
+                               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                               <p>Excepteur sint occaecat cupidatat non  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non.</p>
+                              
+                              
+                               
+                             </div>
+                        </div>
+
+               </div> 
+               <hr>
+
+                <h6>The best lawyers</h6>
+
+               <ul class="filter-result">                 
+                  <li>
+                    <div class="left-step">
+                      <div class="media">
+                         <img class="md-img" src="https://image.freepik.com/free-photo/smiling-mature-lawyer-working-courtroom_23-2147898545.jpg" alt="image">
+                          <div class="media-body">
+                             <h5>Robert Hooks</h5>
+                             <p class="dg-text"><em>Business Lawyers</em></p>
+                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="right-step">
+                      <br>
+                           <a href="javascript:void(0)" class="shrt-btn vw-btn short-font">Send Invitation</a>
+                        </div>  
+                    
+                  </li>
+
+
+                  <li>
+                    <div class="left-step">
+                      <div class="media">
+                         <img class="md-img" src="https://www.bestattorneysofamerica.com/wp-content/uploads/cache/images/for-attorneys-woman/for-attorneys-woman-1416831204.jpg" alt="image">
+                          <div class="media-body">
+                             <h5>Sara Najjar</h5>
+                             <p class="dg-text"><em>Business Lawyers</em></p>
+                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="right-step">
+                      <br>
+                          <a href="javascript:void(0)" class="shrt-btn vw-btn short-font">Send Invitation</a>
+                        </div>  
+                    
+                  </li>
+
+                  <li>
+                    <div class="left-step">
+                      <div class="media">
+                         <img class="md-img" src="https://www.jangattorneys.com/wp-content/themes/jang-2017/img/defaultinnerimgmob.jpg" alt="image">
+                          <div class="media-body">
+                             <h5>Mitri Valentine </h5>
+                              <p class="dg-text"><em>Business Lawyers</em></p>
+                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="right-step">
+                      <br>
+                          <a href="javascript:void(0)" class="shrt-btn vw-btn short-font">Send Invitation</a>
+                        </div>  
+                    
+                  </li>
+
+                  <li>
+                    <div class="left-step">
+                      <div class="media">
+                         <img class="md-img" src="https://freedomlaw.ca/wp-content/uploads/2018/06/corporate.jpg" alt="image">
+                          <div class="media-body">
+                             <h5>William David.</h5>
+                             <p class="dg-text"><em>Business Lawyers</em></p>
+                             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="right-step">
+                      <br>
+                          <a href="javascript:void(0)" class="shrt-btn vw-btn short-font">Send Invitation</a>
+                        </div>  
+                    
+                  </li>
+
+                  
+                  
+               </ul>             
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
