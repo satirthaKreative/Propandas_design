aecb7f8d2f113555927a47b6e54e73f7d9209eef
satirthaKreative <satirtha.kreative@gmail.com>
2020-05-27 19:29:25 +0530

    system notify page design

---
+++ invitation-lawyer.php
@@ -0,0 +1,130 @@
+<?php include ("inc/header.php") ?>
+<!-- <section class="inner-banner">
+   <div class="container">
+      <h1>About Us</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>About Us </li>
+         </ul>
+      </div>
+   </div>
+   </section> -->
+<section class="dshbord-theme">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h3 class="fs-title">Lawyer Invitation</h3>
+            <hr>
+         </div>
+         <div class="col-md-3">
+            <div class="side-menu">
+               <ul>
+                  <li class="active"><a href="javascript:void(0)"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comments" aria-hidden="true"></i>Messages</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file-text" aria-hidden="true"></i>Documents</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-briefcase" aria-hidden="true"></i>Posted Jobs</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-file" aria-hidden="true"></i>Invoices</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-exchange" aria-hidden="true"></i>Transactions</a></li>
+                  <li><a href="javascript:void(0)"><i class="fa fa-comment" aria-hidden="true"></i>System Messages</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- end of col-md-3 -->
+         <div class="col-md-9">
+            <div class="dsbrd-content">
+               <h6>Lawyer Invitation</h6>
+               <ul class="invite-part">                 
+                  <li> 
+                  	<div class="left-step">
+                  	<div class="media">
+                  			 <img class="md-img" src="images/no-img.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>Robert Hooks</h5>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  </div>  
+
+                  <div class="right-step">
+                  	<br>
+                  		   <span> <input type="button" name="" value="Accept" class="shrt-btn acpt-btn "></span>
+                  		   <span> <input type="button" name="" value="Decline" class="shrt-btn declin-btn"></span>
+                        </div>
+                  </li>
+
+
+                  <li> 
+                  	<div class="left-step">
+                  	<div class="media">
+                  			 <img class="md-img" src="images/no-img.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>Sara Najjar</h5>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  </div>  
+
+                  <div class="right-step">
+                  	<br>
+                  		   <span> <input type="button" name="" value="Accept" class="shrt-btn acpt-btn "></span>
+                  		   <span> <input type="button" name="" value="Decline" class="shrt-btn declin-btn"></span>
+                        </div>                	
+                  		
+                  	 
+                  </li>
+
+                  <li> 
+                  	<div class="left-step">
+                  	<div class="media">
+                  			 <img class="md-img" src="images/no-img.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>Mitri Valentine</h5>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  </div>  
+
+                  <div class="right-step">
+                  	<br>
+                  		   <span> <input type="button" name="" value="Accept" class="shrt-btn acpt-btn "></span>
+                  		   <span> <input type="button" name="" value="Decline" class="shrt-btn declin-btn"></span>
+                        </div>                	
+                  		
+                  	 
+                  </li>
+
+                  <li> 
+                  	<div class="left-step">
+                  	<div class="media">
+                  			 <img class="md-img" src="images/no-img.jpg" alt="image">
+                  			  <div class="media-body">
+                  			  	 <h5>William David</h5>
+                  			  	 <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  			  </div>
+                  		</div>
+                  </div>  
+
+                  <div class="right-step">
+                  	<br>
+                  		   <span> <input type="button" name="" value="Accept" class="shrt-btn acpt-btn "></span>
+                  		   <span> <input type="button" name="" value="Decline" class="shrt-btn declin-btn"></span>
+                        </div>                	
+                  		
+                  	 
+                  </li>
+
+                  
+
+                 
+
+                  
+                  
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- end of dshbord-theme -->
+<?php include ("inc/footer.php") ?>
