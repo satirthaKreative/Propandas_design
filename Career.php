25254a2c974792776f03b6dcc555f1144aef2bec
satirthaKreative <satirtha.kreative@gmail.com>
2020-06-10 10:58:53 +0530

    privacy page design

---
+++ Career.php
@@ -0,0 +1,61 @@
+<?php include ("inc/header.php") ?>
+<section class="inner-banner">
+   <div class="container">
+      <h1>Careers</h1>
+      <div class="breadcum-top">
+         <ul class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li>Careers</li>
+         </ul>
+      </div>
+   </div>
+</section>
+<!-- end of inner-banner -->
+
+<section class="basic-content theme-inner">
+   <div class="container">
+      <div class="top-title">
+         <h1>Careers<span></span></h1> 
+         <p>Are you ready to disrupt a highly protected and traditional industry? Join the legal tech movement.</p>       
+      </div>
+
+      <div class="col-md-8 offset-2 text-center">
+      	<p>Send us your elevator pitch and become part of Propandas!</p>
+
+      	<div class="contact-form">
+               <form>
+                  <div class="row">
+                     <div class="col-lg-6 plr-5">             
+                        <input type="text" name="" value="" class="form-control" placeholder="Name">
+                     </div>
+                     <div class="col-lg-6 plr-5">             
+                        <input type="email" name="" value="" class="form-control" placeholder="Email">
+                     </div>
+                     <div class="col-lg-6 plr-5">
+                        <select name="type" id="type" class="form-control" aria-hidden="true">
+                           <option selected="selected" value="mine">Regarding</option>
+                           <option value="">Option 1</option>
+                           <option value="">Option 2</option>
+                           <option value="">Option 3</option>
+                        </select>
+                     </div>
+                     <div class="col-lg-6 plr-5">             
+                        <input type="text" name="" value="" class="form-control" placeholder="Subject">
+                     </div>
+                     <div class="col-lg-12 plr-5">
+                        <textarea name="comment" rows="2" class="form-control" placeholder="Your Message "></textarea>
+                     </div>
+                     <div class="col-lg-12 plr-5">
+                        <input type="submit" value="submit">
+                     </div>
+                  </div>
+               </form>
+            </div>
+      </div>
+
+      
+   </div>
+</section>  <!-- end of basic-content -->
+
+
+<?php include ("inc/footer.php") ?>
