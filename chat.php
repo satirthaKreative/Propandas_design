363055b61fb26fb1c86bf55817a91bf382a593b0
satirthaKreative <satirtha.kreative@gmail.com>
2020-06-16 17:17:48 +0530

    chat page design

--- chat.php
+++ chat.php
@@ -127,7 +127,7 @@
          <div class="mesgs">
             <div class="mesg-top">
                <div class="msg-top-left">
-                  <h6>Project Name</h6>
+                  <h6 >Project Name</h6>
                </div>
                <div class="user-dtls">
                   <ul>
@@ -181,107 +181,12 @@
                      <div class="shrt-view">
                         <ul>
                            <li><a href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i class="fa fa-times-circle-o" aria-hidden="true"></i></a></li>
-                           <li><a href="#" data-toggle="tooltip" data-placement="top" title="Start a Reply"><i class="fa fa-commenting-o" aria-hidden="true"></i></a></li>
+                           <li><a href="#" class="reply-thread"  data-toggle="tooltip" data-placement="top" title="Start a Reply"><i class="fa fa-commenting-o" aria-hidden="true"></i></a></li>
                            <li><a href="#" data-toggle="tooltip" data-placement="top" title="42kb"><i class="fa fa-file-image-o" aria-hidden="true"></i></a></li>
                         </ul>
                      </div>
                   </div>
-                  <div class="reply-box">
-                     <div class="close-box">
-                        <a href="#" data-toggle="tooltip" data-placement="top" title="close"><i class="fa fa-times" aria-hidden="true"></i></a>
-                     </div>
-                     <div class="rply-content">
-                        <div class="front-msg">
-                           <div class="usg-img">
-                              <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> 
-                           </div>
-                           <div class="cont-msg">
-                              <div class="usg-name">
-                                 <h6><a href="#"><strong>User Name</strong></a> <span class="date-usg">11:01 AM | June 9</span></h6>
-                                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                 </p>
-                                 <p><a href="#">https://www.cupidatat.com/images/excepteur.png</a></p>
-                                 <ul>
-                                    <li>Excepteur sint occaecat cupidatat non proident</li>
-                                    <li>Aute irure dolor in reprehenderit in voluptate velit esse</li>
-                                    <li>Sed do eiusmod tempor incididunt ut labore</li>
-                                 </ul>
-                                 <div class="upload-kit">
-                                    <ul>
-                                       <li>
-                                          <img src="images/blog-2.jpg" alt="images" class="img-fluid">
-                                          <div class="upld-btn">
-                                             <span><a href="javascript:void(0)"><i class="fa fa-cloud-upload" aria-hidden="true"></i></a></span>
-                                          </div>
-                                       </li>
-                                       <li>
-                                          <video controls>
-                                             <source src="https://files.slack.com/files-pri/TKJL2HUJK-F014HJVN64T/untitled__may_29_2020_8_38_pm.webm" type="video/mp4">
-                                          </video>
-                                          <div class="upld-btn">
-                                             <span><a href="javascript:void(0)"><i class="fa fa-cloud-upload" aria-hidden="true"></i></a></span>
-                                          </div>
-                                       </li>
-                                    </ul>
-                                 </div>
-                              </div>
-                              <div class="shrt-view">
-                                 <ul>
-                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i class="fa fa-times-circle-o" aria-hidden="true"></i></a></li>
-                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="42kb"><i class="fa fa-file-image-o" aria-hidden="true"></i></a></li>
-                                 </ul>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="front-msg">
-                           <div class="reply-count">
-                              <p>1 replies</p>
-                              <hr>
-                           </div>
-                           <div class="usg-img">
-                              <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> 
-                           </div>
-                           <div class="cont-msg">
-                              <div class="usg-name">
-                                 <h6><a href="#"><strong>User Name</strong></a> <span class="date-usg">11:01 AM | June 9</span></h6>
-                                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.
-                                 </p>
-                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                 </p>
-                              </div>
-                              <div class="shrt-view">
-                                 <ul>
-                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i class="fa fa-times-circle-o" aria-hidden="true"></i></a></li>
-                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="42kb"><i class="fa fa-file-image-o" aria-hidden="true"></i></a></li>
-                                 </ul>
-                              </div>
-                           </div>
-                        </div>
-
-                        <div class="type_msg">
-                        <div class="input_msg_write">
-                           <form action="">
-                              <textarea name="" class="form-control write_msg" placeholder="Type your message..."></textarea>
-                              <div class="setd-tag">
-                                 <div class="attach-icn" data-toggle="tooltip" data-placement="top" title="Attach file">
-                                    <input type="file" name="">
-                                    <span class="file-span"><i class="fa fa-paperclip" aria-hidden="true"></i></span>
-                                 </div>
-                                 <button class="msg_send_btn" type="submit"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                              </div>
-                           </form>
-                        </div>
-                     </div>
-                     </div>
-                     
-                  </div>  <!-- end of reply-box -->
+                   
                </div>
                <div class="front-msg">
                   <div class="usg-img">
@@ -428,6 +333,113 @@
                </div>
             </div>
          </div>
+
+
+         <div class="mesgs reply-box">                     
+                     <div class="mesg-top">
+               <div class="msg-top-left">
+                  <h6>Project Name</h6>
+               </div>
+               <div class="user-dtls">
+                 <div class="close-box">
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="close"><i class="fa fa-times" aria-hidden="true"></i></a>
+                     </div>
+               </div>
+            </div>
+                     <div class="msg_history rply-content ">
+                        <div class="front-msg">
+                           <div class="usg-img">
+                              <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> 
+                           </div>
+                           <div class="cont-msg">
+                              <div class="usg-name">
+                                 <h6><a href="#"><strong>User Name</strong></a> <span class="date-usg">11:01 AM | June 9</span></h6>
+                                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                 </p>
+                                 <p><a href="#">https://www.cupidatat.com/images/excepteur.png</a></p>
+                                 <ul>
+                                    <li>Excepteur sint occaecat cupidatat non proident</li>
+                                    <li>Aute irure dolor in reprehenderit in voluptate velit esse</li>
+                                    <li>Sed do eiusmod tempor incididunt ut labore</li>
+                                 </ul>
+                                 <div class="upload-kit">
+                                    <ul>
+                                       <li>
+                                          <img src="images/blog-2.jpg" alt="images" class="img-fluid">
+                                          <div class="upld-btn">
+                                             <span><a href="javascript:void(0)"><i class="fa fa-cloud-upload" aria-hidden="true"></i></a></span>
+                                          </div>
+                                       </li>
+                                       <li>
+                                          <video controls>
+                                             <source src="https://files.slack.com/files-pri/TKJL2HUJK-F014HJVN64T/untitled__may_29_2020_8_38_pm.webm" type="video/mp4">
+                                          </video>
+                                          <div class="upld-btn">
+                                             <span><a href="javascript:void(0)"><i class="fa fa-cloud-upload" aria-hidden="true"></i></a></span>
+                                          </div>
+                                       </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                              <div class="shrt-view">
+                                 <ul>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i class="fa fa-times-circle-o" aria-hidden="true"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="42kb"><i class="fa fa-file-image-o" aria-hidden="true"></i></a></li>
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="front-msg">
+                           <div class="reply-count">
+                              <p>1 replies</p>
+                              <hr>
+                           </div>
+                           <div class="usg-img">
+                              <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> 
+                           </div>
+                           <div class="cont-msg">
+                              <div class="usg-name">
+                                 <h6><a href="#"><strong>User Name</strong></a> <span class="date-usg">11:01 AM | June 9</span></h6>
+                                 <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat.
+                                 </p>
+                                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                 </p>
+                              </div>
+                              <div class="shrt-view">
+                                 <ul>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="Remove"><i class="fa fa-times-circle-o" aria-hidden="true"></i></a></li>
+                                    <li><a href="#" data-toggle="tooltip" data-placement="top" title="42kb"><i class="fa fa-file-image-o" aria-hidden="true"></i></a></li>
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+
+                        <div class="type_msg">
+                        <div class="input_msg_write">
+                           <form action="">
+                              <textarea name="" class="form-control write_msg" placeholder="Type your message..."></textarea>
+                              <div class="setd-tag">
+                                 <div class="attach-icn" data-toggle="tooltip" data-placement="top" title="Attach file">
+                                    <input type="file" name="">
+                                    <span class="file-span"><i class="fa fa-paperclip" aria-hidden="true"></i></span>
+                                 </div>
+                                 <button class="msg_send_btn" type="submit"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                              </div>
+                           </form>
+                        </div>
+                     </div>
+                     </div>
+                     
+                  </div>
+
+                  <!-- end of reply-box -->
       </div>
    </div>
    <!-- add Modal -->
@@ -470,8 +482,23 @@
 <?php include ("inc/footer.php") ?>
 <script>
    $(document).ready(function(){
-     $('[data-toggle="tooltip"]').tooltip();   
+     $('[data-toggle="tooltip"]').tooltip();     
    });
 
    $('.msg_history').scrollTop($('.msg_history')[0].scrollHeight);
 </script>
+
+ <script>
+         $(document).ready(function(){        
+         $(".reply-thread").click(function(){
+           $(".message-content").addClass("side-reply");
+         });
+         $(".close-box").click(function(){
+            $(".message-content").removeClass("side-reply");
+            });
+         });
+         
+      </script>
+
+
+
