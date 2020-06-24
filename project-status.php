f18beb64fa4a911f4d839ff4646eb0895e9c9168
satirthaKreative <satirtha.kreative@gmail.com>
2020-06-24 12:10:27 +0530

    single lawyer page design

--- project-status.php
+++ project-status.php
@@ -24,7 +24,135 @@
          <div class="col-md-9">
             <div class="dsbrd-content">
 
-              <div class="detail-content prj-status">
+             <div class="table-responsive theme-table prj-list-table">
+          <table class="table table-striped ">
+            <thead>
+            <tr>
+              <th>Project List</th>
+              <th>Project Name</th>
+              <th>Date</th>
+              <th>Status</th>             
+              <th>Action</th>              
+            </tr>
+            </thead>
+            <tbody>
+              <tr>
+            <td> 01</td>
+            <td>Trademark Search Services</td>
+            <td>01/13/2020</td>
+            <td>progress</td>           
+            <td><a target="_blank" href="javascript:void(0)" class="shrt-btn vw-btn short-font" data-toggle="modal" data-target="#review-modal">Completed</a></td>
+          </tr>
+
+          <tr>
+           <td> 02</td>
+            <td>Trademark Search Services</td>
+            <td>01/13/2020</td>
+            <td>progress</td>           
+            <td><a target="_blank" href="javascript:void(0)" class="shrt-btn vw-btn short-font" data-toggle="modal" data-target="#review-modal">Completed</a></td>
+          </tr>
+
+           <tr>
+           <td> 03</td>
+            <td>Trademark Search Services</td>
+            <td>01/13/2020</td>
+            <td>progress</td>           
+            <td><a target="_blank" href="javascript:void(0)" class="shrt-btn vw-btn short-font" data-toggle="modal" data-target="#review-modal">Completed</a></td>
+          </tr>
+
+           <tr>
+           <td> 04</td>
+            <td>Trademark Search Services</td>
+            <td>01/13/2020</td>
+            <td>progress</td>           
+            <td><a target="_blank" href="javascript:void(0)" class="shrt-btn vw-btn short-font" data-toggle="modal" data-target="#review-modal">Completed</a></td>
+          </tr>
+
+           <tr>
+           <td> 05</td>
+            <td>Trademark Search Services</td>
+            <td>01/13/2020</td>
+            <td>progress</td>           
+            <td><a target="_blank" href="javascript:void(0)" class="shrt-btn vw-btn short-font" data-toggle="modal" data-target="#review-modal">Completed</a></td>
+          </tr>
+            </tbody>
+
+          </table>
+         </div>
+
+           
+           <!-- review-modal Modal -->
+   <div class="modal fade theme-modal" id="review-modal">
+      <div class="modal-dialog">
+         <div class="modal-content">
+            <!-- Modal body -->
+            <div class="modal-body ">
+               <div class="prj-review">                     
+                         <div class="rtang-part">
+                           <h6>Write Your Own Review</h6>
+                          
+                           <div class="rating-bx">
+                            <h4>How do you rate this Project? Select the number of stars as your rating</h4> 
+                           <p><span class="rt-type">Service </span>
+                            <span class="strs-rate">
+                              <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>       
+                            </span>
+                           </p>  
+
+                            <p><span class="rt-type">Value</span> 
+                            <span class="strs-rate">
+                              <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>       
+                            </span>
+                           </p>
+
+                            <p><span class="rt-type">Time</span> 
+                            <span class="strs-rate">
+                              <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>       
+                            </span>
+                           </p>                           
+                                                        
+                             </div>
+
+                             <div class="review-bx">
+                              <form>
+                                <div class="form-group">
+                                  <h4>Summary</h4> 
+                                 <input type="text" name="" value="" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                  <h4>Review</h4> 
+                                <textarea class="form-control" ></textarea>
+                                </div>
+
+                                 <div class="form-group">
+                                  <input type="submit" name="" value="Submit" class="flt-search">
+                                </div>
+                                 
+                              </form>
+                               
+                             </div>
+                         </div>
+                      
+                  </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+               <!-- <div class="detail-content prj-status">
                 <div class="row">
                   <div class="col-md-12 text-center">
                      <h5>Trademark Search Services</h5>                     
@@ -32,7 +160,7 @@
                      <h6><span>Date :</span><label>20/04/2020</label></h6>
                       <p class="top-prsp-btn text-center"><span><a href="javascript:void(0)" class="dt-btn cmplted-btn">Completed</a></span> </p>                    
                  
-                  </div>
+                  </div> 
 
                   <div class="col-md-12 prj-review">                     
                          <div class="rtang-part">
@@ -93,11 +221,11 @@
                              </div>
                          </div>
                       
-                  </div>
+                  </div>                 
                   
                 </div> 
                             
-              </div>                       
+              </div> -->                 
                
             </div>
          </div>
